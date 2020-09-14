dead7eb6d88fa49d50fca8f3b6ae851ef45af612
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-09-14 18:12:16 -0400

    redireccion si esta autenticado

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Route::get('/', "LoginController@index")->name("/");
+Route::get('/', "LoginController@index")->name("/")->middleware('guest');
 Route::get("/logout", "LoginController@logout");
 Route::post("/login", "LoginController@login");
 
