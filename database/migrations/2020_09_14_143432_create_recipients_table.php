6a177de010e740b896a26db897ad639a6496155b
Victor Moreno <vamzfe@gmail.com>
2020-09-14 12:50:15 -0400

    Se migrate recipients

---
+++ database/migrations/2020_09_14_143432_create_recipients_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('country_id')->nullable();
+            $table->string('name');
+            $table->string("business_name");
+            $table->string('email')->unique();
+            $table->string('phone',15);
+            $table->text('address');
+            $table->text('address2')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('postcode',10)->nullable();
+            $table->boolean('is_international');//(true si es internacional, false si es nacional)
+            $table->timestamps();
+            //relations
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipients');
+    }
+}
